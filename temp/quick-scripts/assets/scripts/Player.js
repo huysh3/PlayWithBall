b008c04afcd6a542c93c674a5acf1eabf90d4d47
Huysh3 <734645956@qq.com>
2018-06-05 11:27:07 +0800

    [U][init]

---
+++ temp/quick-scripts/assets/scripts/Player.js
@@ -0,0 +1,128 @@
+(function() {"use strict";var __module = CC_EDITOR ? module : {exports:{}};var __filename = 'preview-scripts/assets/scripts/Player.js';var __require = CC_EDITOR ? function (request) {return cc.require(request, require);} : function (request) {return cc.require(request, __filename);};function __define (exports, require, module) {"use strict";
+cc._RF.push(module, '6b0c5ISUY5Ks61Cq5v1layf', 'Player', __filename);
+// scripts/Player.js
+
+'use strict';
+
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+  extends: cc.Component,
+
+  properties: {
+    // 跳跃高度
+    jumpHeight: 0,
+    // 跳跃持续
+    jumpDuration: 0,
+    // 最大移速
+    maxMoveSpeed: 0,
+    // 加速度
+    accel: 0
+
+    // foo: {
+    //     // ATTRIBUTES:
+    //     default: null,        // The default value will be used only when the component attaching
+    //                           // to a node for the first time
+    //     type: cc.SpriteFrame, // optional, default is typeof default
+    //     serializable: true,   // optional, default is true
+    // },
+    // bar: {
+    //     get () {
+    //         return this._bar;
+    //     },
+    //     set (value) {
+    //         this._bar = value;
+    //     }
+    // },
+  },
+
+  setJumpAction: function setJumpAction() {
+    var jumpUp = cc.moveBy(this.jumpDuration, cc.p(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
+    var jumpDown = cc.moveBy(this.jumpDuration, cc.p(0, -this.jumpHeight)).easing(cc.easeCubicActionIn());
+
+    return cc.repeatForever(cc.sequence(jumpUp, jumpDown));
+  },
+  setInputControl: function setInputControl() {
+    var _this = this;
+
+    var self = this;
+
+    // 监听键盘
+    cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, function (event) {
+      switch (event.keyCode) {
+        case cc.KEY.a:
+          _this.accLeft = true;
+          break;
+        case cc.KEY.d:
+          _this.accRight = true;
+          break;
+        default:
+          console.log('啥都不是');
+      }
+    });
+    // 松开按键
+    cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, function (event) {
+      switch (event.keyCode) {
+        case cc.KEY.a:
+          self.accLeft = false;
+          break;
+        case cc.KEY.d:
+          self.accRight = false;
+          break;
+      }
+    });
+  },
+
+
+  // LIFE-CYCLE CALLBACKS:
+
+  onLoad: function onLoad() {
+    this.jumpAction = this.setJumpAction();
+    this.node.runAction(this.jumpAction);
+
+    // 加速度
+    this.accLeft = false;
+    this.accRight = false;
+
+    this.xSpeed = 0;
+
+    this.setInputControl();
+  },
+  start: function start() {},
+  update: function update(dt) {
+    // 根据加速度更新
+    if (this.accLeft) {
+      this.xSpeed -= this.accel * dt;
+    } else if (this.accRight) {
+      this.xSpeed += this.accel * dt;
+    }
+
+    if (Math.abs(this.xSpeed) > this.maxMoveSpeed) {
+      this.xSpeed = this.maxMoveSpeed * this.xSpeed / Math.abs(this.xSpeed);
+    }
+
+    this.node.x += this.xSpeed * dt;
+  }
+});
+
+cc._RF.pop();
+        }
+        if (CC_EDITOR) {
+            __define(__module.exports, __require, __module);
+        }
+        else {
+            cc.registerModuleFunc(__filename, function () {
+                __define(__module.exports, __require, __module);
+            });
+        }
+        })();
+        //# sourceMappingURL=Player.js.map
+        
